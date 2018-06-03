6242da6f9ccc0a0558242093bf10c6054d66d269
John Elliott <johnelliott703@gmail.com>
2018-06-02 21:13:51 -0400

    wip async pug series

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 require('dotenv').config()
-const debug = require('debug')('starter:index')
+
+const debug = require('debug')('fb:index')
+debug.log = console.info.bind(console) // rebind to console.info for firebase functions
+
 const path = require('path')
 const express = require('express')
 const functions = require('firebase-functions')
+const pug = require('pug')
 
+const getTemplate = name => path.join(__dirname, 'views', name + '.pug')
+const eventualData = (input) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(input), Math.floor(Math.random() * 4000))
+  }).then(data => {
+    debug(data)
+    return data
+  })
+}
 var app = express()
 app.set('env', process.env.NODE_ENV)
 app.set('views', path.join(__dirname, 'views'))
@@ -17,10 +30,65 @@ app.get('/status', function (req, res, next) {
 // Static server
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Render index.pug
-app.get('/', function (req, res, next) {
-  res.render('index')
+app.get('/rp', function (req, res, next) {
+  debug('🤖')
+  res.set('Content-Type', 'text/html; charset=utf-8')
+  const write = chunk => {
+    return new Promise((resolve, reject) => {
+      res.write(chunk, 'utf-8', err => {
+        if (err) return reject(err)
+        resolve()
+      })
+    })
+  }
+
+  // const pugPromise = (template, data) => {
+  //   return new Promise((resolve, reject) => {
+  //     res.render(template, data, (err, html) => {
+  //       if (err) return reject(err)
+  //       resolve(html)
+  //     })
+  //   })
+  // }
+
+  const data = [
+    [getTemplate('header'), Promise.resolve({})],
+    [getTemplate('middle'), eventualData({ text: 'DATA_TWO' })],
+    [getTemplate('middle'), eventualData({ text: 'DATA_THREE' })],
+    [getTemplate('middle'), eventualData({ text: 'DATA_4' })],
+    [getTemplate('middle'), eventualData({ text: 'DATA_5' })],
+    [getTemplate('footer'), Promise.resolve({})]
+  ]
+
+  data.reduce((previous, [template, dataPromise]) => {
+    return previous.then(result => {
+      if (result !== '__RENDER_START__') {
+        write(result)
+      }
+      return dataPromise.then(data => {
+        return pug.renderFile(template, data)
+      })
+    })
+  }, Promise.resolve('__RENDER_START__'))
+    .then(write).then(() => {
+      debug('got to res.end')
+      res.status(200)
+      res.end()
+    }).catch(next)
 })
+
+// app.get('/', function (req, res, next) {
+//   res.render('header', (err, string) => {
+//     if (err) next(err)
+//     res.write(string)
+//     // Add dynamic content
+//     res.write('<a href="/pants">click here to go to pants</a>')
+//     res.render('footer', (err, html) => {
+//       if (err) next(err)
+//       res.end(html)
+//     })
+//   })
+// })
 
 // Not found handler
 app.use(function (req, res, next) {
@@ -29,11 +97,14 @@ app.use(function (req, res, next) {
 
 // Error handler
 app.use(function (err, req, res, next) {
+  debug('global error handler')
   debug(err)
-  res.status(500)
-  if (app.get('env') === 'development') {
-    res.type('text/plain')
-    return res.send(err.stack)
+  if (!res.headersSent) {
+    res.status(500)
+    if (app.get('env') === 'development') {
+      res.type('text/plain')
+      return res.send(err.stack)
+    }
   }
   res.end()
 })
